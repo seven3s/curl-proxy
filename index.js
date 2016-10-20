d25891004c2bb329f8cf82f3198fd4856f6ef2b7
花夏 <liubiao0810@live.cn>
2016-10-20 17:00:56 +0800

    修复返回实际statusCode bug

--- index.js
+++ index.js
@@ -15,20 +15,24 @@ module.exports = {
         var method = me.req.method;
         var url = me.req.headers.referer + me.req.url;
         console.log(chalk.green(method + ':') + url);
-        var cmdStr = 'curl -X ' + method + curlParams + ' "' + url + '"';
+        var cmdStr = 'curl -X ' + method + ' -s -w %{http_code}' + curlParams + ' "' + url + '"';
         var contentType = me.req.headers['content-type'];
         var encoding = !(/charset/.test(contentType)) ? 'utf-8' : (function () {
             var s = contentType.search(/charset/);
             return contentType.substr(s).split('=')[1];
         })();
         exec(cmdStr, function(err, stdout, stderr) {
+            var len = stdout.length;
+            // 获取实际的statusCode
+            var statusCode = stdout.slice(-3, len);
+            var data = stdout.slice(0, -3);
             if (err) {
                 console.log('远程代理失败:' + stderr);
             } else {
-                me.res.writeHead(me.res.statusCode, {
+                me.res.writeHead(statusCode, {
                     'Content-Type': contentType
                 });
-                me.res.write(stdout, encoding);
+                me.res.write(data, encoding);
                 me.res.end();
             }
         });
